1c0f07f71636745ed54b1074ea40ff7a1bd18dcf
Célio Henrique Pimenta Júnior <celiohenrique3434@gmail.com>
2026-02-02 13:46:44 -0300

    melhoria no login

--- frontend/src/services/index.js
+++ frontend/src/services/index.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import {auth} from "../config/firebase";
 
 export const api = axios.create({
     baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000/",
@@ -7,4 +8,16 @@ export const api = axios.create({
     headers: {
         "Content-Type": "application/json",
     },
+});
+
+api.interceptors.request.use(async (config) => {
+    const user = auth.currentUser;
+    if (user) {
+        // Pega o token atualizado do usuário logado
+        const token = await user.getIdToken();
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+}, (error) => {
+    return Promise.reject(error);
 });
